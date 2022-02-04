02bae3cd0d5b4e7fddf6303d451a8a6822a06c79
OmarMWarraich <earthling986@gmail.com>
2022-02-04 15:27:04 -0500

    initial commit
    setup tailwindcss and a random image card

---
+++ tailwind_image_gallery/src/App.jsx
@@ -0,0 +1,23 @@
+
+
+function App() {
+
+  return (
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div className="px-6 px-4">
+        <div className="font-bold text-purple-500 text-xl">
+          Photo by John-Doe
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            4000
+          </li>
+        </ul>
+      </div>
+          </div>
+  )
+}
+
+export default App
