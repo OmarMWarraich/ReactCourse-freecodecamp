4af05636629bcd667118c1fc9e89fc6839c8a8af
OmarMWarraich <earthling986@gmail.com>
2022-02-06 00:34:26 +0500

    finished image gallery
    created components and functionality added

--- react-tailwind-pixabay/src/App.js
+++ react-tailwind-pixabay/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -8,46 +10,22 @@ function App() {
   useEffect(() => {
     fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`)
       .then(res => res.json())
-      .then(data => console.log(data))
-      .catch(err => console.error(err))
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err))
   }, [term]);
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl">
-          Photo by John-Doe
-        </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            4000
-          </li>
-          <li>
-            <strong>Views: </strong>
-            300
-          </li>
-          <li>
-            <strong>Views: </strong>
-            400
-          </li>
-        </ul>
-        <div className="px-6 py-4">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1
-                          text-sm font-semibold text-gray-700 mr-2">
-                            #tag1
-                          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1
-                          text-sm font-semibold text-gray-700 mr-2">
-                            #tag2
-                          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1
-                          text-sm font-semibold text-gray-700 mr-2">
-                            #tag3
-                          </span>
-        </div>
-      </div>
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && <h1 className="text-6xl text-center mx-auto mt-32">No Images Found</h1>}
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </div>}
     </div>
   );
 }
