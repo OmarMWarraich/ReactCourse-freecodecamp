e1852609df5129e6b2ae41ab400875ad52d6a0fe
OmarMWarraich <earthling986@gmail.com>
2022-02-06 17:09:56 +0500

    initial commit
    udacity project1 myreads using tailwindcss

---
+++ myreads/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-6xl text-center mx-auto mt-32 underline">hello</h1>
+    </div>
+  );
+}
+
+export default App;
